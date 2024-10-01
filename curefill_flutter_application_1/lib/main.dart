4e4d130b389b6bd7387354e72919ad5b509df012
Elise <eliselh2@illinois.edu>
2024-10-01 14:18:19 -0500

    google maps CIF water station example

--- curefill_flutter_application_1/lib/main.dart
+++ curefill_flutter_application_1/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' as gmaps;
+// import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,18 +57,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
+  late gmaps.GoogleMapController mapController;
 
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
+  final gmaps.LatLng _center = const gmaps.LatLng(40.112442053296135, -88.22833394110455);
+
+  void _onMapCreated(gmaps.GoogleMapController controller) {
+    mapController = controller;
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -76,51 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     // TRY THIS: Try changing the color here to a specific color (to
-    //     // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-    //     // change color while the other colors stay the same.
-    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    //     // Here we take the value from the MyHomePage object that was created by
-    //     // the App.build method, and use it to set our appbar title.
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     // Center is a layout widget. It takes a single child and positions it
-    //     // in the middle of the parent.
-    //     child: Column(
-    //       // Column is also a layout widget. It takes a list of children and
-    //       // arranges them vertically. By default, it sizes itself to fit its
-    //       // children horizontally, and tries to be as tall as its parent.
-    //       //
-    //       // Column has various properties to control how it sizes itself and
-    //       // how it positions its children. Here we use mainAxisAlignment to
-    //       // center the children vertically; the main axis here is the vertical
-    //       // axis because Columns are vertical (the cross axis would be
-    //       // horizontal).
-    //       //
-    //       // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-    //       // action in the IDE, or press "p" in the console), to see the
-    //       // wireframe for each widget.
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         const Text(
-    //           'You have pushed the button this many times:',
-    //         ),
-    //         Text(
-    //           '$_counter',
-    //           style: Theme.of(context).textTheme.headlineMedium,
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: _incrementCounter,
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.add),
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
+    
     return DefaultTabController(length: 4, child: Scaffold(
         appBar: AppBar(
             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -136,12 +90,32 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
-          body: const TabBarView(
+          body: TabBarView(
             children: [
-              Icon(Icons.chat), // temporary fillers for each tab's body
-              Icon(Icons.water_drop),
-              Icon(Icons.location_pin),
-              Icon(Icons.settings),
+              const Icon(Icons.chat), // temporary fillers for each tab's body
+              const Icon(Icons.water_drop),
+              Scaffold(
+                // appBar: AppBar(
+                //   title: const Text('Sydney'), backgroundColor: Colors.green[700]),
+                  body: gmaps.GoogleMap(
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: gmaps.CameraPosition(
+                      target: _center,
+                      zoom: 20.0,
+                    ),
+                  markers: {
+                    const gmaps.Marker(
+                      markerId: gmaps.MarkerId('CIF'),
+                      position: gmaps.LatLng(40.112442053296135, -88.22833394110455),
+                      infoWindow: gmaps.InfoWindow(
+                        title: "CIF",
+                        snippet: "Closest Refill Station: 3rd floor, north side",
+                      ),
+                    )
+                  },
+                ),
+              ),
+              const Icon(Icons.settings),
             ],
           ),
       )
