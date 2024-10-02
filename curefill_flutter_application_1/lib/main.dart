104ddb20167d293329d4e27ffa8b1b98eda41165
maxtan-huynh <maxtan.huynh@gmail.com>
2024-10-02 10:15:41 -0500

    added light/dark mode button, take 2?

--- curefill_flutter_application_1/lib/main.dart
+++ curefill_flutter_application_1/lib/main.dart
@@ -1,45 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart' as gmaps;
-// import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Track the theme mode (light/dark)
+  bool _isDarkMode = false;
+
+  // Method to toggle between light and dark mode
+  void _toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: _isDarkMode
+          ? ThemeData.dark().copyWith(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            )
+          : ThemeData.light().copyWith(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+      home: MyHomePage(
+        title: 'CURefill',
+        toggleTheme: _toggleTheme, // Pass the toggle function to the home page
       ),
-      home: const MyHomePage(title: 'CURefill'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title, required this.toggleTheme});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -51,6 +58,7 @@ class MyHomePage extends StatefulWidget {
   // always marked "final".
 
   final String title;
+  final VoidCallback toggleTheme; // Add a toggleTheme callback
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -59,37 +67,39 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late gmaps.GoogleMapController mapController;
 
-  final gmaps.LatLng _center = const gmaps.LatLng(40.112442053296135, -88.22833394110455);
+  final gmaps.LatLng _center =
+      const gmaps.LatLng(40.112442053296135, -88.22833394110455);
 
   void _onMapCreated(gmaps.GoogleMapController controller) {
     mapController = controller;
   }
-  
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // by the _toggleTheme method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    
-    return DefaultTabController(length: 4, child: Scaffold(
-        appBar: AppBar(
+
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
             title: const Text("CURefill"),
           ),
-        bottomNavigationBar: BottomAppBar(child: 
-          const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.chat)),
-              Tab(icon: Icon(Icons.water_drop)),
-              Tab(icon: Icon(Icons.location_pin)),
-              Tab(icon: Icon(Icons.settings))
-            ],
+          bottomNavigationBar: const BottomAppBar(
+            child: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.chat)),
+                Tab(icon: Icon(Icons.water_drop)),
+                Tab(icon: Icon(Icons.location_pin)),
+                Tab(icon: Icon(Icons.settings))
+              ],
+            ),
           ),
-        ),
           body: TabBarView(
             children: [
               const Icon(Icons.chat), // temporary fillers for each tab's body
@@ -97,16 +107,17 @@ class _MyHomePageState extends State<MyHomePage> {
               Scaffold(
                 // appBar: AppBar(
                 //   title: const Text('Sydney'), backgroundColor: Colors.green[700]),
-                  body: gmaps.GoogleMap(
-                    onMapCreated: _onMapCreated,
-                    initialCameraPosition: gmaps.CameraPosition(
-                      target: _center,
-                      zoom: 20.0,
-                    ),
+                body: gmaps.GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: gmaps.CameraPosition(
+                    target: _center,
+                    zoom: 20.0,
+                  ),
                   markers: {
                     const gmaps.Marker(
                       markerId: gmaps.MarkerId('CIF'),
-                      position: gmaps.LatLng(40.112442053296135, -88.22833394110455),
+                      position:
+                          gmaps.LatLng(40.112442053296135, -88.22833394110455),
                       infoWindow: gmaps.InfoWindow(
                         title: "CIF",
                         snippet: "Closest Refill Station: 3rd floor, north side",
@@ -115,10 +126,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   },
                 ),
               ),
-              const Icon(Icons.settings),
+              // Settings tab with a button to toggle light/dark mode
+              Center(
+                child: ElevatedButton(
+                  onPressed: widget.toggleTheme, // Toggle light/dark mode
+                  child: const Text('Switch Light/Dark Mode'),
+                ),
+              ),
             ],
           ),
-      )
-    );
+        ));
   }
 }
